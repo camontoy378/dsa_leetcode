9d087165e580668822b50c238df42b7963da4ee9
Carlos M <pelao378@gmail.com>
2025-10-06 18:47:20 -0700

    Add LC 402: Remove K Digits
    Add my Python code.
    Wrong Answer. 19/44 test cases passed.
    Remove k contiguous digits.
    Create number using low and high digits.
    Compare if it's the smallest.

---
+++ tests/test_lc_402_remove_k_digits.py
@@ -0,0 +1,48 @@
+import setup_paths
+
+from lc_402_remove_k_digits import RemoveKDigits
+
+def test_solve():
+
+    #Test 1
+
+    number  = "1432219"
+    k       = 3
+    output  = "1219"
+
+    solution = RemoveKDigits(number, k)
+
+    assert solution.solve() == output
+
+    #Test 2
+
+    number  = "10200"
+    k       = 1
+    output  = "200"
+
+    solution = RemoveKDigits(number, k)
+
+    assert solution.solve() == output
+
+    #Test 3
+
+    number  = "10"
+    k       = 2
+    output  = "0"
+
+    solution = RemoveKDigits(number, k)
+
+    assert solution.solve() == output
+
+
+def test_solve_4():
+
+    #Test 4
+
+    number  = "33526221184202197273"
+    k       = 19
+    output  = "0"
+
+    solution = RemoveKDigits(number, k)
+
+    assert solution.solve() == output

087cb6198c9ecaa28b481e42bf524c208aeeb453
Carlos M <pelao378@gmail.com>
2026-03-01 01:46:12 -0800

    Update LC 111: Min Depth Binary Tree
    Updated Python code.
    Added BFS solution.

--- src/lc_111_min_depth_bt.py
+++ src/lc_111_min_depth_bt.py
@@ -1,16 +1,72 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class MinDepthBT():
 
     def __init__(self, bt):
         self.bt = bt
 
+    def get_adj_dict(self, adj_dict, root_node):
+        
+        #Stop condition
+        if root_node == None:
+            return
+        
+        #Work: Fill Adjency dictionary
+        if (root_node.left == None) and (root_node.right == None):
+            adj_dict[root_node.val] = []
+        elif  (root_node.left == None):
+            adj_dict[root_node.val] = [root_node.right.val]
+        elif  (root_node.right == None):
+            adj_dict[root_node.val] = [root_node.left.val]
+        else:
+            adj_dict[root_node.val] = [root_node.left.val, root_node.right.val]
 
-    def get_min_depth(self, node):
+        #Recursion
+        self.get_adj_dict(adj_dict, root_node.left)
+        self.get_adj_dict(adj_dict, root_node.right)
+
+        return
+
+    def get_min_depth_bfs(self, to_visit_dict, node_val):
+        
+        #Setup
+        visiting_q  = []
+
+        #Map Node value : level
+        output      = {}
+
+
+        #Default
+        visiting_q.append(node_val)
+        output[node_val] = 1
+
+
+        while visiting_q:
+            
+            visiting_val = visiting_q.pop(0)
+            
+            #If leaf node (Node with no children).
+            if to_visit_dict[visiting_val] == []:
+                return output[visiting_val]
+
+            for child in to_visit_dict[visiting_val]:
+
+                visiting_q.append(child)
+                output[child] = output[visiting_val] + 1
+
+        return 0
+
+    def get_min_depth_dfs(self, node):
         
         if node == None:
             return None
         
-        left    = self.get_min_depth(node.left)
-        right   = self.get_min_depth(node.right)
+        left    = self.get_min_depth_dfs(node.left)
+        right   = self.get_min_depth_dfs(node.right)
 
         if (left == None) and (right == None):
             return 1
@@ -21,11 +77,25 @@ class MinDepthBT():
         else:
             return 1 + min(left, right)
 
-    def solve(self):
+    def solve_dfs(self):
 
         root = self.bt
 
-        if len(root) == 0:
+        if root == None:
             return 0
 
-        return self.get_min_depth(root)
+        return self.get_min_depth_dfs(root)
+    
+    def solve_bfs(self):
+
+        adj_dict = {}
+
+        root = self.bt
+
+        if root == None:
+            return 0
+        
+        self.get_adj_dict(adj_dict, root)
+
+        return self.get_min_depth_bfs(adj_dict, root.val)
+   
--- tests/test_lc_111_min_depth_bt.py
+++ tests/test_lc_111_min_depth_bt.py
@@ -1,14 +1,76 @@
 import setup_paths
 
 from lc_111_min_depth_bt import MinDepthBT
+from lc_111_min_depth_bt import TreeNode
 
-
-def test_solve():
+def test_get_adj_dict():
 
     #Test 1
-    root    = [3,9,20,None,None,15,7]
+    input   = [3,9,20,None,None,15,7]
     output  = 2
+
+    adj_dict = {}
+
+    root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
 
     solution = MinDepthBT(root)
 
-    #assert solution.solve() == True
+    solution.get_adj_dict(adj_dict, root)
+
+    assert adj_dict[3]  == [9,20]
+    assert adj_dict[20] == [15,7]
+    assert adj_dict[15] == []
+
+    #Test 2
+    input   = [2,None,3,None,4,None,5,None,6]
+    output  = 5
+
+    adj_dict = {}
+
+    root = TreeNode(2, right=TreeNode(3, right=TreeNode(4, right=TreeNode(5, right=TreeNode(6)  ) )) )
+
+    solution = MinDepthBT(root)
+
+    solution.get_adj_dict(adj_dict, root)
+
+    assert adj_dict[2] == [3]
+    assert adj_dict[5] == [6]
+    assert adj_dict[6] == []
+
+def test_solve_bfs():
+
+    #Test 1
+    input   = [3,9,20,None,None,15,7]
+    output  = 2
+
+    root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+
+    solution = MinDepthBT(root)
+
+    assert solution.solve_bfs() == output
+
+    #Test 2
+    input   = [2,None,3,None,4,None,5,None,6]
+    output  = 5
+
+    adj_dict = {}
+
+    root = TreeNode(2, right=TreeNode(3, right=TreeNode(4, right=TreeNode(5, right=TreeNode(6)  ) )) )
+
+    solution = MinDepthBT(root)
+
+    assert solution.solve_bfs() == output
+
+
+
+def test_solve_dfs():
+
+    #Test 1
+    input   = [3,9,20,None,None,15,7]
+    output  = 2
+
+    root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+
+    solution = MinDepthBT(root)
+
+    assert solution.solve_dfs() == output
